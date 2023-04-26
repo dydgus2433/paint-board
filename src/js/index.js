554d8dbe3e7d5a374f1431d37cfbc96d90541256
yh <dydgus2433@naver.com>
2023-04-26 22:12:34 +0900

    지우개 기능 추가

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,14 @@ class PaintBoard {
   brushEl;
   context;
   colorPickerEl;
+  eraserEl;
+  eraserColor = "#FFFFFF";
+  backgroundColor = "#FFFFFF";
   IsMouseDown = false;
   constructor() {
     this.assignElement();
     this.initContext();
+    this.initCanvasBackgroundColor();
     this.addEvent();
   }
 
@@ -23,12 +27,17 @@ class PaintBoard {
     this.brushSliderEl = this.brushPanelEl.querySelector("#brushSize");
     this.brushSizePreviewEl =
       this.brushPanelEl.querySelector("#brushSizePreview");
+    this.eraserEl = this.toolbarEl.querySelector("#eraser");
   }
 
   initContext() {
     this.context = this.canvasEl.getContext("2d");
   }
 
+  initCanvasBackgroundColor() {
+    this.context.fillStyle = this.backgroundColor;
+    this.context.fillRect(0, 0, this.canvasEl.width, this.canvasEl.height);
+  }
   addEvent() {
     this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
     this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
@@ -42,6 +51,17 @@ class PaintBoard {
       this.onChangeBrushSize.bind(this)
     );
     this.colorPickerEl.addEventListener("input", this.onChangeColor.bind(this));
+    this.eraserEl.addEventListener("click", this.onClickEraser.bind(this));
+  }
+
+  onClickEraser(event) {
+    console.log("onClickEraser");
+    const isActive = event.currentTarget.classList.contains("active");
+    this.MODE = isActive ? "NONE" : "ERASER";
+    this.canvasEl.style.cursor = isActive ? "default" : "crosshair";
+    this.brushPanelEl.classList.add("hide");
+    this.eraserEl.classList.toggle("active");
+    this.brushEl.classList.add("hide");
   }
 
   onMouseOut() {
@@ -70,8 +90,13 @@ class PaintBoard {
     this.context.beginPath();
     this.context.moveTo(currentPosition.x, currentPosition.y);
     this.context.lineCap = "round";
-    this.context.strokeStyle = this.colorPickerEl.value;
-    this.context.lineWidth = this.brushSliderEl.value;
+    if (this.MODE === "BRUSH") {
+      this.context.strokeStyle = this.colorPickerEl.value;
+      this.context.lineWidth = this.brushSliderEl.value;
+    } else if (this.MODE === "ERASER") {
+      this.context.strokeStyle = this.eraserColor;
+      this.context.lineWidth = 50;
+    }
   }
 
   onMouseMove(event) {
